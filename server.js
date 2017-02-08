65acc125b519b2e55b751de783114dc25a8e2f39
gaganpg96 <gaganpg96@gmail.com>
2017-02-08 11:30:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/style.css',function(req,res){
+app.get('/article-one',function(req,res){
     res.send("article one requested and will be served");
 });
-app.get('/ui/style.css',function(req,res){
+app.get('/article-two',function(req,res){
     res.send("article two requested and will be served");
 });
-app.get('/ui/style.css',function(req,res){
+app.get('/ui/article-three',function(req,res){
     res.send("article three requested and will be served");
 });
 app.get('/ui/madi.png', function (req, res) {
